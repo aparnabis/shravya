59fe5de8255be6cdf84b84cecfa970d4d39b02e3
aparnabis <59922975+aparnabis@users.noreply.github.com>
2020-07-31 19:28:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, pig1,pig3;
+var backgroundImg,platform;
+var bird, slingshot;
+
+
+function preload() {
+    backgroundImg = loadImage("sprites/bg.png");
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+ 
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+ 
+    box5 = new Box(810,160,70,70);
+
+    bird = new Bird(200,200);
+ 
+}
+
+function draw(){
+    background(backgroundImg);
+    Engine.update(engine);
+    //strokeWeight(4);
+    box1.display();
+    box2.display();
+    ground.display();
+    
+    box3.display();
+    box4.display();
+    bird.display();
+    box5.display();
+    
+}
+
